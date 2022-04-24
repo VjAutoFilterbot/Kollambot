0d78b83c4f7b6eb6ed4be5620b98ffb9ef9c5a75
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-04-24 21:09:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/VjAutoFilterbot/Kollambot.git /Kollambot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Kollambot
 fi
-cd /EvaMaria
+cd /Kollambot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
